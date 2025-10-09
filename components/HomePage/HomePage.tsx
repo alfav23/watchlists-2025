6d5b0c9fdb2eb145c8b9142d8291a6884aea4818
Alyssa Favorito <alyssafavorito2000@gmail.com>
2025-10-09 01:00:04 -0700

    setup

---
+++ components/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+'use client';
+import styles from "./HomePage.module.scss";
+
+export const HomePage = () => {
+    return (
+        <div className={styles.container}></div>
+    )
+}
+
+export default HomePage;
