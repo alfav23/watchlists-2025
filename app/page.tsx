3997c95f86f4c8ed6119b8c21ff9e0d17ffc63e3
Alyssa Favorito <alyssafavorito2000@gmail.com>
2025-10-09 01:26:24 -0700

    API changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+import styles from "./page.module.scss";
 import HomePage from "@/components/HomePage/HomePage";
 
 export default function Home() {
