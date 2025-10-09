b9549214286d24e646fadc1d4ae17a7ab393b19d
Alyssa Favorito <alyssafavorito2000@gmail.com>
2025-10-09 06:20:31 -0700

    discover side-bar

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,11 +1,12 @@
 'use client';
 import styles from "./HomePage.module.scss";
 import Feed from "../Feed";
+import Discover from '../Discover';
 
 export const HomePage = () => {
     return (
         <div className={styles.container}>
-            Main Content
+            <Discover />
             <Feed />
         </div>
     )
