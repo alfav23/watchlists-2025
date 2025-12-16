0617dc3cae8c72d4dbc0ac50e09b1f60c0cb7dba
Alyssa Favorito <alyssafavorito2000@gmail.com>
2025-12-16 13:20:06 -0500

    responsiveness polishing, add search param use state to discover component

--- components/Discover/Discover.tsx
+++ components/Discover/Discover.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import styles from "./Discover.module.scss";
 import { FaSearch } from "react-icons/fa";
 
 export const Discover = () => {
+
+    const [searchParam, setSearchParam ] = useState<string>("");
+
+
     return (
         <div className={styles.discoverContainer}>
             <div className={styles.discoverTitle}>Discover</div>
             <div className={styles.searchContainer}>
-                <input className={styles.search} type="text" placeholder="Search"/>
+                <input className={styles.search} type="text" placeholder="Search" value={searchParam} onChange={(e) => setSearchParam(e.target.value)}/>
                 <div className={styles.searchIcon}>
                 <FaSearch />  
                 </div>
